32ef8f7e0c6e4320fe7939d0f650002defc96ae2
Myo Min Htet <myominhtet771454093@gmail.com>
2024-06-16 15:55:08 +0630

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,15 @@ forEach((button) => {
               quantity : 1
             });
            }
+
+           let cartQuantity =0;
+
+           cart.forEach((item) => {
+                cartQuantity += item.quantity;
+           });
+
+           document.querySelector('.js-cart-quantity').innerHTML
+           = cartQuantity;
           
-          console.log(cart);
      });
 });
