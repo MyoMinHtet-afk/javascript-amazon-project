74626c631785fc094639c26d5e2b76dd2825f91e
Myo Min Htet <myominhtet771454093@gmail.com>
2024-06-17 12:58:29 +0630

    Move add to cart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart,addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML='';
@@ -59,6 +59,21 @@ products.forEach((product) => {
     `
 });
 
+
+
+function updateCartQuantity(){
+
+      let cartQuantity =0;
+
+      cart.forEach((cartItem) => {
+          cartQuantity += cartItem.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML
+      = cartQuantity; 
+ 
+}
+
 document.querySelector(".js-products-grid").innerHTML=productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').
@@ -68,31 +83,7 @@ forEach((button) => {
           const productId = button.dataset
           .productId;
 
-           let matchingItem;
-
-           cart.forEach((item) => {
-               if(productId === item.productId){
-                matchingItem = item;
-               }
-           });
-
-           if(matchingItem){
-            matchingItem.quantity += 1;
-           }else{
-            cart.push({
-              productId: productId,
-              quantity : 1
-            });
-           }
-
-           let cartQuantity =0;
-
-           cart.forEach((item) => {
-                cartQuantity += item.quantity;
-           });
-
-           document.querySelector('.js-cart-quantity').innerHTML
-           = cartQuantity;
-          
+           addToCart(productId);
+           updateCartQuantity();
      });
 });
