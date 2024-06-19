b0ebc845aa58458a1b534294781f79560ab08a1e
Myo Min Htet <myominhtet771454093@gmail.com>
2024-06-19 15:40:25 +0630

    rename

---
+++ jasmine-standalone-5.1.1/data/testCart.js
@@ -0,0 +1,10 @@
+import { addToCart, cart} from "../../data/cart.js";
+
+describe('test suite : addToCart',() => {
+  it ('add an existing product to cart',() => {
+       document.querySelector('.js-test-container')
+  });
+
+
+
+});
