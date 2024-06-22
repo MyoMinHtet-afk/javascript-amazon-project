2bfc28194d3853ac24e555391eaaf2c6d3dbd9f5
Myo Min Htet <myominhtet771454093@gmail.com>
2024-06-22 09:15:08 +0630

    Load products from backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,10 @@
 import { cart,addToCart } from "../data/cart.js";
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid(){
 let productsHTML='';
 
 products.forEach((product) => {
@@ -90,3 +93,4 @@ forEach((button) => {
            updateCartQuantity();
      });
 });
+}
